c2dee5117a716d64ee3ccd8c338b8b6c32dc7e56
dianacg1 <diana.carreno@kellogg.northwestern.edu>
2022-01-13 03:20:53 +0000

    lab four point one

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -3,4 +3,11 @@
 -- Expected result:
 -- 2935
 
+-- SELECT players.id, players.first_name, players.last_name, stats.hits, stats.id,
+-- FROM stats INNER JOIN id on id.players = id.stats 
+-- WHERE first_name.players = "Barry" AND last_name.players = "Bonds";
 
+SELECT SUM(stats.hits)
+FROM players INNER JOIN stats ON players.id = stats.player_id
+WHERE first_name = "Barry"
+AND last_name = "Bonds";
